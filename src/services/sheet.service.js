37343b1b5e1d2f3f3e0ea17bcd5a4b1336d68e73
Gabriel Carneiro <gabriel.carneiro97@live.com>
2019-01-25 09:43:52 -0200

    Acabei a parte da API do Google

--- src/services/sheet.service.js
+++ src/services/sheet.service.js
@@ -1,4 +1,11 @@
+const google = require('googleapis');
+
+const { authorize } = require('./gApi.service');
+
 const COLS_CONFIG = require('../../columns.json');
+const { sheetId: spreadsheetId } = require('../../config.json');
+
+const sheets = new google.GoogleApis().sheets('v4');
 
 function ntol(num) {
   let ret = '';
@@ -27,9 +34,54 @@ function calcCol(mes, dec) {
   return ntol(c + primNum);
 }
 
-console.log(calcCol('13', 'esocial'));
+function getSheetRange(range) {
+  return new Promise((resolve, reject) => {
+    authorize((auth) => {
+      sheets.spreadsheets.values.get({
+        auth,
+        range,
+        spreadsheetId,
+      }, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  });
+}
+
+function writeSheetRange(range, values) {
+  return new Promise((resolve, reject) => {
+    authorize((auth) => {
+      sheets.spreadsheets.values.update({
+        auth,
+        range,
+        valueInputOption: 'RAW',
+        resource: {
+          values,
+        },
+        spreadsheetId,
+      }, (err, res) => {
+        if (err) reject(err);
+        else resolve(res);
+      });
+    });
+  });
+}
+
+function writeCell(cellId, data) {
+  return writeSheetRange(cellId, [[data]]);
+}
+
+// getSheetRange('2019!A1').then((res) => {
+//   let rows = res.data.values;
+
+//   console.log(rows);
+// }).catch(err => console.error(err));
+
+writeCell('2019!A3', 'EITCHA!').then(r => console.log(r)).catch(err => console.error(err));
 
 module.exports = {
   lton,
   ntol,
+  calcCol,
 };
